1b9554e85ad35561e8b6b2b6409cbe4e21dc9bd1
zhangyemengren <624593758@qq.com>
2023-12-09 22:29:47 +0800

    feat: update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::{Html, Json}, routing::get, Router};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/api/users", get(test_user))
+        .route("/*all", get(catch_all));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
@@ -15,4 +19,23 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn catch_all() -> Html<&'static str> {
+    Html("<h1>404</h1>")
+}
+async fn test_user() -> Json<Value> {
+    Json(json!({
+        "data":[
+            {
+                "id": 1,
+                "name": "John Doe",
+            },
+            {
+                "id": 2,
+                "name": "Miles Davis",
+            },
+        ],
+        "total": 2,
+    }))
 }
