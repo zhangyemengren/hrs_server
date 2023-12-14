2e0ce98bd1d21bb975c7e68c1f8ce71b9b4d44a3
zhangyemengren <624593758@qq.com>
2023-12-14 19:15:13 +0800

    feat: update

--- crates/macro/tests/macro_test.rs
+++ crates/macro/tests/macro_test.rs
@@ -4,8 +4,8 @@ use hrs_server_macros::{i18n, t};
 fn test_t() {
     assert_eq!(t!(), "This is my macro!");
 }
-i18n!("..");
-// i18n!();
+// i18n!("..");
+i18n!();
 #[test]
 fn test_proc() {
     i18n_content();
--- crates/proc_macro/src/lib.rs
+++ crates/proc_macro/src/lib.rs
@@ -1,8 +1,12 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, parse::Parse, LitStr};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+use syn::{parse::Parse, parse_macro_input, LitStr};
 
-struct Args{
+struct Args {
     path: Option<LitStr>,
 }
 
@@ -13,9 +17,8 @@ impl Parse for Args {
         } else {
             Some(input.parse()?)
         };
-        Ok(Args{ path })
+        Ok(Args { path })
     }
-
 }
 
 #[proc_macro]
@@ -27,15 +30,37 @@ pub fn i18n(input: TokenStream) -> TokenStream {
         "locales".to_string()
     };
     let mut current_dir = std::env::current_dir().unwrap();
-    let f: std::path::PathBuf = j.into();
-    current_dir.push(&f);
-    println!("current dir: {} {:?}", current_dir.canonicalize().unwrap().display(), f);
+    current_dir.push::<PathBuf>(j.into());
+    let path = current_dir.canonicalize().unwrap();
+    let mut jmap = HashMap::new();
+    let x = fs::read_dir(path).unwrap();
+    x.fold(&mut jmap, |acc, x| {
+        let x = x.unwrap();
+        let path = x.path();
+        if path.is_dir() {
+            let name = path.file_name().unwrap().to_str().unwrap().to_string();
+            let path = path.to_str().unwrap();
+            let dir = fs::read_dir(path).unwrap();
+            let second_map2 = dir.fold(HashMap::new(), |mut acc, x| {
+                let x = x.unwrap();
+                let path = x.path();
+                let name = path.file_stem().unwrap().to_str().unwrap().to_string();
+                let path = path.to_str().unwrap();
+                let content = fs::read_to_string(path).unwrap();
+                let json_content: Value = serde_json::from_str(&content).unwrap();
+                acc.insert(name, json_content);
+                acc
+            });
+            acc.insert(name.to_string(), second_map2);
+        }
+        acc
+    });
+    println!("{:?}", jmap);
     generate_code().into()
 }
 
 fn generate_code() -> proc_macro2::TokenStream {
     quote! {
-        use std::path::PathBuf;
 
         pub fn i18n_content() {
             println!("i18n_content");
