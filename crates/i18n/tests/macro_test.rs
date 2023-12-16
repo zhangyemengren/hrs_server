07245ea3903944400c667a6bc6f9668e817176d3
zhangyemengren <624593758@qq.com>
2023-12-16 12:13:18 +0800

    feat: 日志

--- crates/i18n/tests/macro_test.rs
+++ crates/i18n/tests/macro_test.rs
@@ -1,6 +1,5 @@
 use hrs_server_macros::{i18n, t};
 
-// i18n!("..");
 i18n!();
 
 #[test]
@@ -8,4 +7,3 @@ fn test_t() {
     assert_eq!(t!("common", "hello"), "Hello World!");
     assert_eq!(t!("common", "hello", "cn"), "你好世界！");
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 use axum::{
+    extract::MatchedPath,
+    http::Request,
     response::{Html, Json},
     routing::get,
     Router,
 };
 use serde_json::{json, Value};
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
         .route("/api/users", get(test_user))
-        .route("/*all", get(catch_all));
+        .route("/*all", get(catch_all))
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
+                // Log the matched route's path (with placeholders not filled in).
+                // Use request.uri() or OriginalUri if you want the real path.
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
+
+                info_span!(
+                    "http_request",
+                    method = ?request.method(),
+                    matched_path,
+                    some_other_field = tracing::field::Empty,
+                )
+            }),
+        );
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
