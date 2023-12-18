3eb0c504ec2885a21f38da551f3dbe24851c8e35
zhangyemengren <624593758@qq.com>
2023-12-18 15:49:13 +0800

    feat: t宏及测试

--- crates/i18n/src/lib.rs
+++ crates/i18n/src/lib.rs
@@ -6,9 +6,33 @@ macro_rules! t {
         t!($name_space, $key, "en")
     }};
     ($name_space:tt, $key:tt, $lang:tt) => {{
-        let j = I18N_MAP.get($name_space).unwrap().get($lang).unwrap();
-        let s = serde_json::from_str::<serde_json::Value>(&j).unwrap();
-        let r = s.get($key).unwrap().as_str().unwrap();
-        r.to_string()
+        match I18N_MAP.get($name_space) {
+            None => {
+                println!("Name Space:{} Not Found", $name_space);
+                "".to_string()
+            }
+            Some(ns) => match ns.get($lang) {
+                None => {
+                    println!("Language Key:{} Not Found", $lang);
+                    "".to_string()
+                }
+                Some(lang_str) => match serde_json::from_str::<serde_json::Value>(lang_str) {
+                    Err(_) => {
+                        println!("Json Parse Error on Name Space:{} Language Key:{} Key:{}", $name_space, $lang, $key);
+                        "".to_string()
+                    }
+                    Ok(json) => match json.get($key) {
+                        None => {
+                            println!("Key:{} Not Found", $key);
+                            "".to_string()
+                        }
+                        Some(v) => v.as_str().unwrap_or_else(|| {
+                            println!("Key:{} Not String", $key);
+                            ""
+                        }).to_string()
+                    }
+                }
+            }
+        }
     }};
 }
--- crates/i18n/tests/macro_test.rs
+++ crates/i18n/tests/macro_test.rs
@@ -6,4 +6,12 @@ i18n!();
 fn test_t() {
     assert_eq!(t!("common", "hello"), "Hello World!");
     assert_eq!(t!("common", "hello", "cn"), "你好世界！");
+    assert_eq!(t!("common", "hello", "jp"), "");
+    assert_eq!(t!("common", "world", "en"), "");
+    assert_eq!(t!("common", "world", "jp"), "");
+    assert_eq!(t!("nonexistent", "hello"), "");
+    assert_eq!(t!("nonexistent", "hello", "cn"), "");
+    assert_eq!(t!("nonexistent", "hello", "jp"), "");
+    assert_eq!(t!("nonexistent", "world", "en"), "");
+    assert_eq!(t!("nonexistent", "world", "jp"), "");
 }
