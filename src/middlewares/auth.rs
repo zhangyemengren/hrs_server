31e57a99fb3de171b6dd77f8640f587ac7a5c97f
zhangyemengren <624593758@qq.com>
2024-01-18 19:24:14 +0800

    feat: 添加jwt

--- src/middlewares/auth.rs
+++ src/middlewares/auth.rs
@@ -3,6 +3,8 @@ use axum::middleware::Next;
 use axum::response::{IntoResponse, Response};
 use axum::{extract, Json};
 use axum_extra::extract::{CookieJar, PrivateCookieJar};
+use jsonwebtoken as jwt;
+use time::{OffsetDateTime, UtcOffset};
 
 const WHITELIST: [&str; 3] = ["/", "/health_check", "/api/v1/login"];
 
@@ -38,4 +40,94 @@ pub async fn auth(
     let response = next.run(request).await;
     println!("cookies: {:?} {:?}", session, jar.get("is_login"));
     Ok(response)
+}
+/// jwt
+///
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Jwt {
+    secret: Vec<u8>,
+    claims: Claims,
+}
+
+#[derive(Debug, Default, serde::Serialize, serde::Deserialize)]
+pub struct Claims {
+    aud: String,
+    sub: String,
+    exp: u64,
+}
+
+impl Jwt {
+    pub fn new() -> Self {
+        let claims = Claims::default();
+        let secret = Vec::new();
+        Self { secret, claims }
+    }
+    pub fn aud(&mut self, aud: &str) -> &mut Self {
+        self.claims.aud = aud.to_owned();
+        self
+    }
+    pub fn sub(&mut self, sub: &str) -> &mut Self {
+        self.claims.sub = sub.to_owned();
+        self
+    }
+    pub fn exp(&mut self, exp: u64) -> &mut Self {
+        self.claims.exp = exp;
+        self
+    }
+    pub fn new_token(&self) -> anyhow::Result<String> {
+        let r = jwt::encode(
+            &jwt::Header::default(),
+            &self.claims,
+            &jwt::EncodingKey::from_secret(self.secret.as_slice()),
+        )?;
+        Ok(r)
+    }
+    pub fn validate(&self, token: &str) -> anyhow::Result<jwt::TokenData<Claims>> {
+        let mut validation = jwt::Validation::default();
+        validation.set_audience(&[self.claims.aud.to_owned()]);
+        validation.sub = Some(self.claims.sub.to_owned());
+        let r = jwt::decode::<Claims>(
+            token,
+            &jwt::DecodingKey::from_secret(self.secret.as_slice()),
+            &validation,
+        )?;
+        Ok(r)
+    }
+}
+
+impl Default for Jwt {
+    fn default() -> Self {
+        let mut j = Self::new();
+        j.secret = b"secret".to_vec();
+        let utc_now = OffsetDateTime::now_utc();
+        // 计算时区
+        let offset = UtcOffset::from_hms(8, 0, 0).unwrap();
+        // 加默认过期间隔
+        let exp = utc_now.to_offset(offset).unix_timestamp() + 1;
+
+        j.claims = Claims {
+            aud: "hrs".to_owned(),
+            sub: "sso".to_owned(),
+            exp: exp as u64,
+        };
+        j
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_jwt() {
+        let j = Jwt::default();
+        let token = j.new_token().unwrap();
+        let data = j.validate(&token).unwrap();
+        assert!(token.len() > 0);
+        assert_eq!(data.claims.aud, "hrs");
+        assert_eq!(data.claims.sub, "sso");
+        // let j = Jwt::default().exp(1);
+        // let token = j.new_token().unwrap();
+        // let data = j.validate(&token).map_err(|e| e.to_string()).unwrap();
+        // println!("{:?}", data);
+    }
 }
